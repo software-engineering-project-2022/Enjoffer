4df0155849dbd5fc483156c983210bc1e6904b50
Volodymyr Dolishnii <106533930+VolodymyrDolishnii@users.noreply.github.com>
2022-10-10 09:39:36 +0300

    Add files via upload

---
+++ Enjoffer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+
+var cs = "Host=localhost;Username=postgres;Password=1111;Database=enjoffer";
+
+using var connection = new NpgsqlConnection(cs);
+connection.Open();
+
+string sql = "SELECT * FROM users";
+using var cmd = new NpgsqlCommand(sql, connection);
+
+using NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+while (rdr.Read())
+{
+    Console.WriteLine("{0} {1} {2}, {3}", rdr.GetInt32(0), rdr.GetString(1),
+            rdr.GetString(2), rdr.GetString(3));
+}
