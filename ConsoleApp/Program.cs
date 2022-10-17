d79b11384b1365f98d24c1f1dd6344484aad41c0
Yaroslav <51023046+YaroslavDZ@users.noreply.github.com>
2022-10-17 05:26:23 +0300

    generation database

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            var manipulate = new ManipulationDB("Host=localhost;Username=postgres;Password=yari2233;Database=enjoffer");
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("1. Insert data tables");
+                    Console.WriteLine("2. Print data");
+                    Console.WriteLine("3. Delete data");
+                    Console.WriteLine("\n");
+
+                    Console.Write("Enter number of task:\t");
+
+                    string num = Console.ReadLine();
+
+                    if (num == "1")
+                    {
+                        Console.Write("Enter number of records:\t");
+                        int n = int.Parse(Console.ReadLine());
+                        manipulate.TablesInsertion(n);
+                        Console.WriteLine("Insertion is done successfully\n");
+                    }
+                    else if (num == "2")
+                    {
+                        manipulate.Print();
+                    }
+                    else if (num == "3")
+                    {
+                        manipulate.TablesDeletion();
+                        Console.WriteLine("Deletion is done successfully\n");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
