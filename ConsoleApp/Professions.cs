34d980774e3efa0e81a3f303e7fe8ae3354fe6df
Yaroslav <51023046+YaroslavDZ@users.noreply.github.com>
2022-10-17 05:52:18 +0300

    console app in order to generate the database

---
+++ ConsoleApp/Professions.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp
+{
+    public class Professions
+    {
+        public readonly string Developer;
+        public readonly string Doctor;
+        public readonly string Mathematician;
+
+        public Professions()
+        {
+            Developer = "Розробник";
+            Doctor = "Терапевт";
+            Mathematician = "Математик";
+        }
+        public static readonly List<string> professions = new List<string>()
+        {
+            new Professions().Developer,
+            new Professions().Doctor,
+            new Professions().Mathematician
+        };
+
+    }
+}
