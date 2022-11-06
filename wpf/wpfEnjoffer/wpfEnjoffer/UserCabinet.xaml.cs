757311b3fc06d6907905cff225a36496b622ab3d
Yaroslav <51023046+YaroslavDZ@users.noreply.github.com>
2022-11-06 13:32:15 +0200

    the part of the user cabinet

--- wpf/wpfEnjoffer/wpfEnjoffer/UserCabinet.xaml.cs
+++ wpf/wpfEnjoffer/wpfEnjoffer/UserCabinet.xaml.cs
@@ -18,9 +18,9 @@ namespace wpfEnjoffer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserCabinet : Window
     {
-        public MainWindow()
+        public UserCabinet()
         {
             InitializeComponent();
         }
